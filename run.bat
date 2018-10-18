cc1366c24dc1e86793948d22645802ac3ffb134f
Reto Weiss <reto.weiss@ivyteam.ch>
2018-10-18 23:06:20 +0200

    Use java 11 to run

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-SET JAVA_HOME=c:\program files\java\jdk1.8.0_112
+SET JAVA_HOME=c:\program files\java\jdk-11
 SET MAVEN=C:\Tools\maven\apache-maven-3.5.2\bin\mvn
 SET MAIN_CLASS=ch.rweiss.terminal.chart.Test
 
